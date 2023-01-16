d02df1ba902ca7d6b3f91a475341e3f80975ce6c
oscar vasquez <ovasquezv@gmail.com>
2023-01-16 17:09:53 -0500

    Tablas de Multiplicar

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Solution {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        int cont = 1;
+        while (cont < 11) {
+            System.out.println(N + " x " + cont + " = " + N * cont);
+            cont++;
+        }
+    }
 }
